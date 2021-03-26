b0de2b382caeef98c147b1d3defb3e6af3964397
Akshay Bhatia <bhatiaks@amazon.com>
2021-03-25 22:27:58 -0700

    Added country code for breaking news by reading context

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -1,6 +1,8 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -9,18 +11,20 @@ import com.androiddevs.mvvmnewsapp.R
 import db.ArticleDatabase
 import kotlinx.android.synthetic.main.activity_news.*
 import repository.NewsRepository
+import java.util.*
 
 class NewsActivity : AppCompatActivity() {
 // API_KEY = b3612bdd00c64118a33acc7e646d17d4
 
     lateinit var viewModel: NewsViewModel
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
         val newsRepository = NewsRepository(ArticleDatabase(this))
-        val locale: String = applicationContext.applicationContext.getResources().getConfiguration().locale.getCountry()
-        val viewModelProviderFactory = NewsViewModelProviderFactory(locale, newsRepository)
+        val locale: Locale = getResources().getConfiguration().locales.get(0)
+        val viewModelProviderFactory = NewsViewModelProviderFactory(locale.country.toString(), newsRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
